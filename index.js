9073e342535b8a490664b1cf95c6345dffd5bd3b
michael2023-code <michael.mutunga@student.moringaschool.com>
2023-07-14 21:45:55 +0300

    done

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ if (ride >2000 && ride <=2500)
 
   else if (ride > 2500){
   return 'No can do.'}
+} 
+
+function ternaryCheckCity(city){
+if (city =="NYC")
+return `Ok, sounds good.`
+ if (city !=="NYC"){
+ return `No go.`
+}
 }
 
 
+function switchOnCharmFromTip(tips) {
 
-
-
-
+  if (tips === "generous") {
+      return `Thank you so much.`
+  } else if (tips === `not as generous`) {
+      return `Thank you.`
+  } else {
+      return `Bye.`
+  }
+}
